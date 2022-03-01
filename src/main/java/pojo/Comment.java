108a064f7ccad6c8f9317d7bb705211e2bb6ab7e
Bulat Nurgaliev <xeroiiin@gmail.com>
2022-03-02 01:35:21 +0300

    Created pojo files and added lombok support

---
+++ src/main/java/pojo/Comment.java
@@ -0,0 +1,23 @@
+package pojo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.cucumber.java.eo.Se;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Comment {
+
+	@JsonProperty("postId")
+	private int postId;
+	@JsonProperty("id")
+	private int id;
+	@JsonProperty("name")
+	private String name;
+	@JsonProperty("email")
+	private String email;
+	@JsonProperty("body")
+	private String body;
+
+}
---
+++ src/main/java/pojo/Post.java
@@ -0,0 +1,20 @@
+package pojo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Post{
+
+	@JsonProperty("userId")
+	private int userId;
+	@JsonProperty("id")
+	private int id;
+	@JsonProperty("title")
+	private String title;
+	@JsonProperty("body")
+	private String body;
+
+}
