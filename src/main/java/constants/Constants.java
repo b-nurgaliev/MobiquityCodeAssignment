992d222f0dbe033729d535fd999de13b2c49f370
Bulat Nurgaliev <xeroiiin@gmail.com>
2022-03-04 01:20:44 +0300

    added constants and configured TestRunner

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -2,4 +2,9 @@ package constants;
 
 public class Constants {
 	public static final String URL = "https://jsonplaceholder.typicode.com";
+	public static final String USERS_URL = "https://jsonplaceholder.typicode.com/users";
+	public static final String POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
+	public static final String COMMENTS_URL = "https://jsonplaceholder.typicode.com/comments";
+	public static final String POSTID_QUERY = "/?postId=";
+
 }
--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/features",
-		glue = {"steps"})
+		glue = {"steps"},
+		plugin={"pretty"},
+		stepNotifications=true)
 public class TestRunner {
 }
