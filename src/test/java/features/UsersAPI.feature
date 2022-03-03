4960e26990baa089e387270d5a5ca9b4ea2200a2
Bulat Nurgaliev <xeroiiin@gmail.com>
2022-03-03 16:03:53 +0300

    added steps with getting userId, posts and post bodies

--- src/test/java/features/UsersAPI.feature
+++ src/test/java/features/UsersAPI.feature
@@ -3,3 +3,9 @@ Feature: Users API
 		When I send GET users request
 		Then Status code is 200
 		And Unique user "Delphine" is found
+		And I collect users ID
+		And I get posts of user by his ID
+		And I get response body for all posts
+		When I create a list of all comments
+#		And I received list of emails
+#		Then I validate emails
--- src/test/java/steps/StepDefinition.java
+++ src/test/java/steps/StepDefinition.java
@@ -5,16 +5,18 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
+
 import io.restassured.response.Response;
+import pojo.Comment;
+import pojo.Post;
 import pojo.User;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
+
 
 import static constants.Constants.URL;
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StepDefinition {
 
@@ -46,5 +48,46 @@ public class StepDefinition {
 				.getBody().as(User[].class);
 		assertEquals(1, (int) Arrays.stream(users)
 				.filter(user -> user.getUsername().equals(username)).count());
+	}
+
+	@And("I collect users ID")
+	public void iCollectUsersID() {
+//		Response response = (Response) context.getContext("rawResponse");
+		Response getDelphine = given()
+				.when()
+				.contentType(ContentType.JSON)
+				.get(URL + "/users/?username=Delphine");
+		User[] users = getDelphine
+				.getBody().as(User[].class);
+		int userId = users[0].getId();
+		context.setContext("userId", userId);
+	}
+
+	@And("I get posts of user by his ID")
+	public void iGetPostsOfUserByHisID() {
+		int userId = (int) context.getContext("userId");
+		Response getPosts = given()
+				.when()
+				.contentType(ContentType.JSON)
+				.get(URL + "/posts/?userId=" + userId);
+		context.setContext("getPosts", getPosts);
+	}
+
+	@And("I get response body for all posts")
+	public void iGetResponseBodyForAllPosts() {
+		Response postBody = (Response) context.getContext("getPosts");
+		Post[] posts = postBody
+				.getBody().as(Post[].class);
+		context.setContext("postBody", posts);
+	}
+
+	@When("I create a list of all comments")
+	public void iCreateAListOfAllComments() {
+		Response commentBody = (Response) context.getContext("postBody");
+		Comment[] comments = commentBody
+				.getBody().as(Comment[].class);
+		context.setContext("commentBody", comments);
+
+		int i = 0;
 	}
 }
