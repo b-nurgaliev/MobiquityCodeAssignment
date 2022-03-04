d3b589987d7408c325d798cc9c6438d1e01a2cc3
Bulat Nurgaliev <xeroiiin@gmail.com>
2022-03-04 23:22:50 +0300

    minor fix

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -6,5 +6,4 @@ public class Constants {
 	public static final String POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
 	public static final String COMMENTS_URL = "https://jsonplaceholder.typicode.com/comments";
 	public static final String POSTID_QUERY = "/?postId=";
-
 }
