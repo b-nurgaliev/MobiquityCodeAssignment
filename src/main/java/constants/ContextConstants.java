74401e1f3972a660ddca1de7a7a45320ee885676
Bulat Nurgaliev <xeroiiin@gmail.com>
2022-03-06 15:29:49 +0300

    created ContextConstants and inserted them into StepDefinition

--- src/main/java/constants/ContextConstants.java
+++ src/main/java/constants/ContextConstants.java
@@ -1,5 +1,13 @@
 package constants;
 
 public class ContextConstants {
-	public static final String RESPONSE = "";
+
+	public static final String RAW_RESPONSE = "rawResponse";
+	public static final String RAW_FOUND_COMMENTS_RESPONSE = "rawFoundCommentsResponse";
+	public static final String RAW_POST_RESPONSE = "rawPostResponse";
+	public static final String RAW_POST_BODY_RESPONSE = "rawPostBodyResponse";
+	public static final String RAW_GET_EMAIL_RESPONSE = "rawGetEmailResponse";
+	public static final String RAW_POST_ID_RESPONSE = "rawPostIdResponse";
+	public static final String RAW_USER_ID_RESPONSE = "rawUserIdResponse";
+
 }
--- src/test/java/steps/StepDefinition.java
+++ src/test/java/steps/StepDefinition.java
@@ -11,12 +11,11 @@ import io.restassured.response.Response;
 import pojo.Comment;
 import pojo.Post;
 import pojo.User;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import static constants.Constants.*;
+import static constants.ContextConstants.*;
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -38,18 +37,18 @@ public class StepDefinition {
 				.when()
 				.contentType(ContentType.JSON)
 				.get(USERS_URL);
-		context.setContext("rawResponse", response);
+		context.setContext(RAW_RESPONSE, response);
 	}
 
 	@Then("Status code is {int}")
 	public void statusCodeIs(int statusCode) {
-		Response response = (Response) context.getContext("rawResponse");
+		Response response = (Response) context.getContext(RAW_RESPONSE);
 		assertEquals(response.getStatusCode(), statusCode);
 	}
 
 	@And("Unique user {string} is found")
 	public void uniqueUserIsFound(String username) {
-		Response response = (Response) context.getContext("rawResponse");
+		Response response = (Response) context.getContext(RAW_RESPONSE);
 		User[] users = response
 				.getBody().as(User[].class);
 		assertEquals(1, (int) Arrays.stream(users)
@@ -65,56 +64,56 @@ public class StepDefinition {
 		User[] users = getDelphine
 				.getBody().as(User[].class);
 		int rawUserIdResponse = users[0].getId();
-		context.setContext("rawUserIdResponse", rawUserIdResponse);
+		context.setContext(RAW_USER_ID_RESPONSE, rawUserIdResponse);
 	}
 
 	@And("I get posts of user by his ID")
 	public void iGetPostsOfUserByHisID() {
-		int rawUserIdResponse = (int) context.getContext("rawUserIdResponse");
+		int rawUserIdResponse = (int) context.getContext(RAW_USER_ID_RESPONSE);
 		Response rawPostResponse = given()
 				.when()
 				.contentType(ContentType.JSON)
 				.get(POSTS_URL + USER_ID + rawUserIdResponse);
-		context.setContext("rawPostResponse", rawPostResponse);
+		context.setContext(RAW_POST_RESPONSE, rawPostResponse);
 	}
 
 	@And("All posts have correct user id")
 	public void iGetResponseBodyForAllPosts() {
-		Response rawPostBodyResponse = (Response) context.getContext("rawPostResponse");
+		Response rawPostBodyResponse = (Response) context.getContext(RAW_POST_RESPONSE);
 		Post[] posts = rawPostBodyResponse
 				.getBody().as(Post[].class);
-		context.setContext("rawPostBodyResponse", posts);
+		context.setContext(RAW_POST_BODY_RESPONSE, posts);
 		assertTrue(Arrays.stream(posts)
-				.allMatch(post -> post.getUserId() == (int) context.getContext("rawUserIdResponse")));
+				.allMatch(post -> post.getUserId() == (int) context.getContext(RAW_USER_ID_RESPONSE)));
 	}
 
 	@And("I create a list of posts")
 	public void iCreateAListOfPosts() {
-		Post[] postsList = (Post[]) context.getContext("rawPostBodyResponse");
+		Post[] postsList = (Post[]) context.getContext(RAW_POST_BODY_RESPONSE);
 		ArrayList<Integer> postIds = new ArrayList<>();
 		postHelper.createPostIdsList(postIds, postsList);
-		context.setContext("rawPostIdResponse", postIds);
+		context.setContext(RAW_POST_ID_RESPONSE, postIds);
 	}
 
 	@When("I create a list of all comments")
 	public void iCreateAListOfAllComments() {
-		ArrayList<Integer> postIds = (ArrayList<Integer>) context.getContext("rawPostIdResponse");
+		ArrayList<Integer> postIds = (ArrayList<Integer>) context.getContext(RAW_POST_ID_RESPONSE);
 		List<Comment> rawFoundCommentsResponse = new ArrayList<>();
 		commentHelper.getCommentsList(postIds, rawFoundCommentsResponse);
-		context.setContext("rawFoundCommentsResponse", rawFoundCommentsResponse);
+		context.setContext(RAW_FOUND_COMMENTS_RESPONSE, rawFoundCommentsResponse);
 	}
 
 	@And("I received list of emails")
 	public void iReceivedListOfEmails() {
-		List<Comment> rawFoundCommentsResponse = (List<Comment>) context.getContext("rawFoundCommentsResponse");
+		List<Comment> rawFoundCommentsResponse = (List<Comment>) context.getContext(RAW_FOUND_COMMENTS_RESPONSE);
 		ArrayList<String> emails = new ArrayList<>();
 		commentHelper.getEmails(emails, rawFoundCommentsResponse);
-		context.setContext("rawGetEmailResponse", emails);
+		context.setContext(RAW_GET_EMAIL_RESPONSE, emails);
 	}
 
 	@Then("All comments have valid emails")
 	public void iValidateEmails() {
-		ArrayList<String> emails = (ArrayList<String>) context.getContext("rawGetEmailResponse");
+		ArrayList<String> emails = (ArrayList<String>) context.getContext(RAW_GET_EMAIL_RESPONSE);
 		commentHelper.checkEmailFormat(emails);
 	}
 
